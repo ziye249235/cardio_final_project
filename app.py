1ae3b454d385c1bc7bd588e19390b68e6850a4c5
ziye249235 <zzyzzy96@hotmail.com>
2020-11-28 02:25:55 -0500

    added navbar and 3 other pages for HTMl website

--- app.py
+++ app.py
@@ -12,11 +12,17 @@ df=pd.read_csv('merged_cardio_train_sample.csv')
 def index():
         return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+@app.route('/sampledata')
+def sampledata():
+    return render_template('sampledata.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
+@app.route('/info')
+def info():
+        return render_template('info.html')
 
+@app.route('/model')
+def model():
+        return render_template('model.html')
 
 
 if __name__ == "__main__":
